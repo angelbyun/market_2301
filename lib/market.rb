6cdc840c7e7cef0730f5b2f99ec9a54d6c5f21e8
Angel Byun <abyun22@turing.edu>
2023-03-07 11:30:49 -0700

    Add initial method and adding vendor method with testing for adding vendor

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,19 @@
 class Market
-  
+  attr_reader :name,
+              :vendors,
+              :vendor_names
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+    vendor_names = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  # def add_vendor_name(vendor_name)
+  #   @vendor_names << vendor_name
+  # end
 end
