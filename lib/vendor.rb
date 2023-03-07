bb68136394f71943fe850410d22e0a855e8c9c4b
Angel Byun <abyun22@turing.edu>
2023-03-07 10:45:05 -0700

    Add check_stock and stock method and testing

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -6,4 +6,12 @@ class Vendor
     @name = name
     @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+    check_stock = 0
+  end
+
+  def stock(item, number_of_items)
+    @inventory[item] += number_of_items
+  end
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -9,5 +9,26 @@ RSpec.describe Vendor do
 
     expect(vendor).to be_a(Vendor)
     expect(vendor.name).to eq('Rocky Mountain Fresh')
+    expect(vendor.inventory).to eq({})
+  end
+
+  it 'checks stock of item' do
+    vendor = Vendor.new('Rocky Mountain Fresh')
+    item1 = Item.new({name: 'Peach', price: '$0.75'})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    expect(vendor.check_stock(item1)).to eq(0)
+  end
+
+  it 'add stock to inventory' do
+    vendor = Vendor.new('Rocky Mountain Fresh')
+    item1 = Item.new({name: 'Peach', price: '$0.75'})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.stock(item1, 30)
+
+    expect(vendor.inventory).to eq({
+      item1 => 30
+    })
   end
 end
