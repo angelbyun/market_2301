2143488d5fc474158c202d70c3837e108bc4bb61
Angel Byun <abyun22@turing.edu>
2023-03-07 11:35:07 -0700

    Add testing for adding inventory to each vendor

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -34,20 +34,32 @@ RSpec.describe Market do
     # expect(market.vendor_names).to eq(['Rocky Mountain Fresh', 'Ba-Nom-a-Nom', 'Palisade Peach Shack'])
   end
 
-  # it 'adds items to vendor inventory' do
-  #   market = Market.new('South Pearl Street Farmers Market')
-  #   vendor1 = Vendor.new("Rocky Mountain Fresh")
-  #   vendor2 = Vendor.new("Ba-Nom-a-Nom")
-  #   vendor3 = Vendor.new("Palisade Peach Shack")
-  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
-  #   item2 = Item.new({name: 'Tomato', price: "$0.50"})
-  #   item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-  #   item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  it 'adds items to vendor inventory' do
+    market = Market.new('South Pearl Street Farmers Market')
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     
-  #   vendor1.stock(item1, 35)
-  #   vendor1.stock(item2, 7)
-  #   vendor2.stock(item4, 50)
-  #   vendor2.stock(item3, 25)
-  #   vendor3.stock(item1, 65)
-  # end
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+
+    expect(vendor1.inventory).to eq({
+      item1 => 35,
+      item2 => 7
+    })
+    expect(vendor2.inventory).to eq({
+      item3 => 25,
+      item4 => 50
+    })
+    expect(vendor3.inventory).to eq({
+      item1 => 65
+    })
+  end
 end
